79574f28826c1ed41693baf7a65f66c65e067b56
Manuel Gomes Rosmaninho <50143720+mgrosmaninho@users.noreply.github.com>
2021-10-13 11:38:40 +0100

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,30 @@
+ass minecraft (
+  $url = 'https://launcher.mojang.com/v1/objects/a0d03225615ba897619220e256a266cb33a44b6b/server.jar',
+  $install_dir = '/opt/minecraft'
+){
+  file { $install_dir:
+    ensure => directory,
+  }
+  file { "${install_dir}/minecraft_server.jar":
+    ensure => file,
+    source => $url,
+  }
+
+  package {'java':
+    ensure => present,
+  }
+  file {"${install_dir}/eula.txt":
+    ensure => file,
+    content => 'eula=true'
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
+    ensure => file,
+    content => epp(‘minecraft/minecraft.service.epp’, {
+      install_dir => $install_dir,
+    }),
+  }
+  service { 'minecraft':
+    ensure => running,
+    enable => true,
+  }
+}
