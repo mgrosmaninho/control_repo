c148c4759cd0c9dc48b88a32bf5e6fa43727d669
Manuel Gomes Rosmaninho <50143720+mgrosmaninho@users.noreply.github.com>
2021-10-13 11:28:34 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,20 @@
 node default {
+  file { '/root/README':
+    ensure  => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
   file { '/root/README':
-    ensure => file,
+    ensure  => file,
     content => "Welcome to ${fqdn}\n",
-    owner => 'root',
+    owner   => 'root',
   }
+}
+node 'minetest.puppet.vm': {
+  include role::minecraft_server
 }
 node /^web/ { 
   include role::app_server
